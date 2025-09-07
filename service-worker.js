930fbfb3321a6299daf10d91b1402470e1401667
kardamanil <87255711+kardamanil@users.noreply.github.com>
2025-09-07 10:36:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,13 +7,11 @@ const urlsToCache = [
   "/JalGanana/manifest.json",
   "/JalGanana/icon.png"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
