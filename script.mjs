4f46f5067f3823298641dc69a79f15d6a4370cb3
kardamanil <87255711+kardamanil@users.noreply.github.com>
2025-09-07 08:45:47 +0530

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-import { db } from "./index.html";
+//import { db } from "./index.js";
 import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 // Base Lab No
